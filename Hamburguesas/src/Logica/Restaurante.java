731c62685d3544e1338a552d214562770a5e3136
unknown <s.diazm1@uniandes.edu.co>
2021-09-05 10:57:56 -0500

    prueba1

--- Hamburguesas/src/Logica/Restaurante.java
+++ Hamburguesas/src/Logica/Restaurante.java
@@ -1,5 +1,18 @@
 package Logica;
 
+
+import java.util.List;
+import modelo.Ingrediente;
+import modelo.Combo;
+
 public class Restaurante {
+	
+	private List menu;
+	
+	private List<Ingrediente> ingrediente;
+	
+	private List<Combo> combo;
+	
+	
 
 }
--- Hamburguesas/src/modelo/Ingrediente.java
+++ Hamburguesas/src/modelo/Ingrediente.java
@@ -1,5 +1,25 @@
 package modelo;
 
 public class Ingrediente {
+	
+	private String nombre;
+	
+	private int costoAdicional;
+	
+	public Ingrediente(String pnombre, int pcostoAdicional) 
+	{
+		nombre = pnombre;
+		costoAdicional=pcostoAdicional;
+	}
+	
+	public String darNombre() 
+	{
+		return nombre;
+	}
+	
+	public int darCostoAdicional()
+	{
+		return costoAdicional;
+	}
 
 }
